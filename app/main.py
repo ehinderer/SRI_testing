0d6c8dae800919c57c03385abc32128827b6cf2e
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2022-05-20 17:22:02 -0700

    remove useless code

--- app/main.py
+++ app/main.py
@@ -2,18 +2,16 @@
 FastAPI web service wrapper for SRI Testing harness
 (i.e. for reports to a Translator Runtime Status Dashboard)
 """
-from typing import Optional, Dict, List
+from typing import Optional, List
+from uuid import uuid4
 from pydantic import BaseModel
 
 import uvicorn
 from fastapi import FastAPI, HTTPException
 
-from reasoner_validator import DEFAULT_TRAPI_VERSION, is_valid_trapi_query
+from reasoner_validator import DEFAULT_TRAPI_VERSION
 from reasoner_validator.util import latest
-from reasoner_validator.biolink import (
-    check_biolink_model_compliance_of_query_graph,
-    check_biolink_model_compliance_of_knowledge_graph
-)
+
 
 app = FastAPI()
 
@@ -48,20 +46,6 @@ class TestRunParameters(BaseModel):
     # KP entry specified x-translator specified model releases.
     biolink_version: Optional[str] = None
 
-
-@app.post("/run_tests")
-async def run_tests(test_parameters: TestRunParameters):
-
-    trapi_version = latest.get(test_parameters.trapi_version)
-    biolink_version = test_parameters.biolink_version
-
-    results: List[str] = list()
-
-    return {
-        "trapi_version": trapi_version,
-        "biolink_version": biolink_version,
-        "validation": results
-    }
 
 
 if __name__ == "__main__":
