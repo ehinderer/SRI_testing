79170389ea3e18b1da5d3ea0c7027f0677e796fd
richardbruskiewich <richard.bruskiewich@delphinai.com>
2022-07-13 00:38:26 -0700

    fleshed out TestResultsDatabase class and some basic unit tests

--- tests/translator/sri/testing/test_report_db.py
+++ tests/translator/sri/testing/test_report_db.py
@@ -1,21 +1,46 @@
 import pytest
 
 from translator.sri.testing.report_db import TestReportDatabase
+from pymongo.errors import ConnectionFailure
+
+DEBUG: bool = True
 
 
-def test_create_test_report_db():
+def test_report_db_connection():
     try:
-        trd = TestReportDatabase()
-    except RuntimeError as rte:
-        assert False, f"Failed to create TestReportDatabase(), exception: {str(rte)}"
+        TestReportDatabase()
+    except ConnectionFailure:
+        assert False, "This test connection should succeed if a suitable Mongodb instance is running?!"
 
 
-@pytest.mark.parametrize(
-    "query",
-    [
-        (),
-        ()
-    ]
-)
-def test_get_edge_details_file_path(query):
-    assert query
+def test_false_report_db_connection():
+    try:
+        TestReportDatabase(
+            user="nobody",
+            password="nonsense",
+            host="neverneverland"
+        )
+        assert False, "This nonsense test connection should always fail!"
+    except ConnectionFailure:
+        assert True
+
+
+def test_insert_test_entry():
+    try:
+        tdb = TestReportDatabase()
+
+        test_item = {
+            "component": "SRI",
+            "activity": "testing"
+        }
+
+        result = tdb.get_collection().insert_one(test_item)
+
+        assert result
+        assert result.inserted_id
+
+        if not DEBUG:
+            tdb.get_collection().delete_one(result.inserted_id)
+
+    except ConnectionFailure:
+        assert False, "This test document insertion should succeed if a suitable Mongodb instance is running?!"
--- translator/sri/testing/report_db.py
+++ translator/sri/testing/report_db.py
@@ -3,45 +3,57 @@ from os import environ
 from urllib.parse import quote_plus
 
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 from pymongo.collection import Collection
 from pymongo.database import Database
 
+import logging
+logger = logging.getLogger()
+logger.setLevel("DEBUG")
+
 RUNNING_INSIDE_DOCKER = environ.get('RUNNING_INSIDE_DOCKER', False)
-
-# TODO: need to figure out how to configure a more robust
-#       user and password, inside the Docker Compose run
-user: str = environ.get('MONGO_INITDB_ROOT_USERNAME', "root")
-password: str = environ.get('MONGO_INITDB_ROOT_PASSWORD', "example")
-host: str = "mongo" if RUNNING_INSIDE_DOCKER else "localhost"
-
-_mongodb_uri = "mongodb://%s:%s@%s" % (quote_plus(user), quote_plus(password), host)
-
-_mongodb_client = MongoClient(
-    host=_mongodb_uri,    # host: Optional[Union[str, Sequence[str]]] = None,
-    port=27017,  # port: Optional[int] = None,
-    # document_class: Optional[Type[_DocumentType]] = None,
-    # tz_aware: Optional[bool] = None,
-    # connect: Optional[bool] = None,
-    # type_registry: Optional[TypeRegistry] = None,
-    # **kwargs: Any,
-)
-
-_sri_testing_db: Database = _mongodb_client.sri_testing
-
-_test_results_collection: Collection = _sri_testing_db.test_results
-
-test_item = {
-    "name": "Richard",
-    "role": "da Boss"
-}
-
-entry_id = _test_results_collection.insert_one(test_item)
-
-# test_results.delete_one(entry_id.inserted_id)
 
 
 class TestReportDatabase:
+    """
+    Wrapper class for a MongoDb instance storing and retrieving SRI Testing test results.
+    """
 
-    def __init__(self):
-        self._mongodb_uri = "mongodb://%s:%s@%s" % (quote_plus(user), quote_plus(password), host)
-        
+    def __init__(
+            self,
+            user: str = environ.get('MONGO_INITDB_ROOT_USERNAME', "root"),
+            password: str = environ.get('MONGO_INITDB_ROOT_PASSWORD', "example"),
+            host: str = "mongo" if RUNNING_INSIDE_DOCKER else "localhost"
+    ):
+        """
+        TestReportDatabase constructor.
+
+        :param user: str, MongoDb user (default: 'MONGO_INITDB_ROOT_USERNAME' or "root")
+        :param password:  str, MongoDb password (default: 'MONGO_INITDB_ROOT_PASSWORD' or "example")
+        :param host:  str, MongoDb host (default: "mongo" if inside the SRI Testing docker container else "localhost")
+
+        :raises pymongo.errors.ConnectionFailure
+        """
+        self._db_uri = "mongodb://%s:%s@%s" % (quote_plus(user), quote_plus(password), host)
+        self._db_client = MongoClient(
+            host=self._db_uri,  # host: Optional[Union[str, Sequence[str]]] = None,
+            port=27017,  # port: Optional[int] = None,
+            # document_class: Optional[Type[_DocumentType]] = None,
+            # tz_aware: Optional[bool] = None,
+            # connect: Optional[bool] = None,
+            # type_registry: Optional[TypeRegistry] = None,
+            # **kwargs: Any,
+        )
+        self._db_client.admin.command('ping')  # will through pymongo.errors.ConnectionFailure if the connection fails
+
+        self._sri_testing_db: Database = self._db_client.sri_testing
+        self._test_results_collection: Collection = self._sri_testing_db.test_results
+
+    def get_client(self):
+        return self._db_client
+
+    def get_database(self):
+        return self._sri_testing_db
+
+    def get_collection(self):
+        return self._test_results_collection
